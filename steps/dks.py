d725d56fe8ce238e2562f8905129165d0cea0caf
Toby Walls <toby.walls@engineering.digital.dwp.gov.ukDD>
2023-01-19 16:03:08 +0000

    make error more specific - ignore empty dbObject

--- steps/dks.py
+++ steps/dks.py
@@ -129,8 +129,8 @@ class MessageCryptoHelper(object):
         record_accumulator: pyspark.Accumulator = None,
     ) -> UCMessage:
 
-        if not message.dbobject:
-            raise ValueError("dbObject empty or missing")
+        if message.dbobject is None:
+            raise ValueError("dbObject missing")
 
         encryption_materials = message.encryption_materials
         data_key = self.data_key_service.decrypt_data_key(
