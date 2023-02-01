34da256893a9d8db1127f0cb66ef204133f0d946
Toby Walls <toby.walls@engineering.digital.dwp.gov.ukDD>
2023-02-01 14:19:50 +0000

    temp schedule for afternoon testing

--- daily_export_scheduling.tf
+++ daily_export_scheduling.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_event_rule" "utc_06_00_daily" {
   count               = local.run_daily_export_on_schedule[local.environment] == true ? 1 : 0
   name                = "corporate_data_ingestion_utc_06_00_daily"
   description         = "6AM every day"
-  schedule_expression = "cron(00 10 ? * * *)"
+  schedule_expression = "cron(00 16 ? * * *)"
 }
 
 resource "aws_cloudwatch_event_target" "run_daily_export" {
