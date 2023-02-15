110d9b0c625971771bf1d11b598063d950c3fc1e
Toby Walls <toby.walls@engineering.digital.dwp.gov.ukDD>
2023-02-15 16:28:16 +0000

    --wip-- [skip ci]

--- daily_export_scheduling.tf
+++ daily_export_scheduling.tf
@@ -2,15 +2,40 @@ resource "aws_cloudwatch_event_rule" "utc_06_00_daily" {
   count               = local.run_daily_export_on_schedule[local.environment] == true ? 1 : 0
   name                = "corporate_data_ingestion_utc_06_00_daily"
   description         = "6AM every day"
-  schedule_expression = "cron(00 06 ? * * *)"
+  schedule_expression = "cron(55 11 ? * * *)"
 }
 
 resource "aws_cloudwatch_event_target" "run_daily_export" {
-  count     = local.run_daily_export_on_schedule[local.environment] == true ? 1 : 0
+  for_each  = local.collections_configuration
   rule      = aws_cloudwatch_event_rule.utc_06_00_daily[0].name
   target_id = aws_sns_topic.corporate_data_ingestion.name
   arn       = aws_sns_topic.corporate_data_ingestion.arn
   input     = <<JSON
-  {"launch_type": "scheduled"}
+  {
+    "s3_overrides": null,
+    "overrides": {
+      "Steps": [{
+        "Name": "corporate-data-ingestion::${each.key}::daily-run",
+        "ActionOnFailure": "CONTINUE",
+        "HadoopJarStep": {
+          "Jar": "command-runner.jar",
+          "Args": [
+            "spark-submit",
+            "/opt/emr/steps/corporate_data_ingestion.py",
+            "--source_s3_prefix",
+            "${each.value["source_s3_prefix"]}",
+            "--destination_s3_prefix",
+            "${each.value["destination_s3_prefix"]}",
+            "--collection_names",
+            "${each.value["collection_names"]}",
+            "--concurrency",
+            "${each.value["concurrency"]}"
+          ]
+        }
+      }]
+    },
+    "extend": null,
+    "additional_step_args": null
+  }
   JSON
 }
