aaa4b3da6181fd7210dc201f8e072012b5d50c2f
Toby Walls <toby.walls@engineering.digital.dwp.gov.ukDD>
2023-02-28 14:47:22 +0000

    Remove unused code

--- steps/utils.py
+++ steps/utils.py
@@ -13,38 +13,6 @@ class ValidationError(BaseException):
 
 class Utils(object):
     @staticmethod
-    def decompress(compressed_text: bytes, accumulator: pyspark.Accumulator) -> bytes:
-        """Decompresses jsonl.gz"""
-        accumulator += 1
-        return gzip.decompress(compressed_text)
-
-    @staticmethod
-    def to_records(record_tuple: Tuple[str, bytes]) -> List[str]:
-        """Decodes, removes empty line from end of each file, and splits by line"""
-        filename = record_tuple[0].split("/")[-1]
-        lines = record_tuple[1].decode().rstrip("\n").split("\n")
-
-        # Extract the offsets and calculate the number of records expected
-        match = re.match(
-            r"^(?P<collection>([a-zA-Z0-9\-_]+\.){1,2}[a-zA-Z0-9\-_]+)"
-            r"[_-](?P<partition>\d+)"
-            r"[_-](?P<start_offset>\d+)"
-            r"[_-](?P<end_offset>\d+)"
-            r"\.jsonl\.gz$",
-            filename,
-        )
-
-        if not match:
-            raise ValueError("Could not retrieve kafka offsets from filename")
-
-        match_groups = match.groupdict()
-        expected_lines = 1 + int(match_groups["end_offset"]) - int(match_groups["start_offset"])
-        if expected_lines != len(lines):
-            raise ValidationError(f"offset indicates '{expected_lines}' records, but '{len(lines)}' records"
-                                  f" read from file: '{filename}'")
-        return lines
-
-    @staticmethod
     def get_decryption_helper(
         decrypt_endpoint: str,
         correlation_id: str,
