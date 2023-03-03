cf02ef6d4982c8d61e1b4eb99373f328e5bb964a
Toby Walls <toby.walls@engineering.digital.dwp.gov.ukDD>
2023-03-03 15:53:10 +0000

    fmt

--- bootstrap_actions.tf
+++ bootstrap_actions.tf
@@ -133,9 +133,9 @@ resource "aws_s3_bucket_object" "metrics_setup_sh" {
   content = templatefile("${path.module}/bootstrap_actions/metrics-setup.sh",
     {
       maven_binary_location = format("s3://%s", data.terraform_remote_state.common.outputs.config_bucket.id)
-      metrics_pom       = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.metrics_pom.key)
-      prometheus_config = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.prometheus_config.key)
-      proxy_url         = data.terraform_remote_state.internal_compute.outputs.internet_proxy.url
+      metrics_pom           = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.metrics_pom.key)
+      prometheus_config     = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.prometheus_config.key)
+      proxy_url             = data.terraform_remote_state.internal_compute.outputs.internet_proxy.url
     }
   )
   tags = {
