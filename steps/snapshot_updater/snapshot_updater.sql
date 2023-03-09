5084aaa916fd748394c1869af2aa7e03737b58c6
Nathan Bigaignon <nathan.bigaignon@engineering.digital.dwp.gov.uk>
2023-03-09 17:15:42 +0000

    Add temporary code to benchmark copying snapshot backup

--- steps/snapshot_updater/snapshot_updater.sql
+++ steps/snapshot_updater/snapshot_updater.sql
@@ -1,9 +1,14 @@
--- This is a placeholder and will be replaced by snapshot_updater code when it is ready
-CREATE TABLE IF NOT EXISTS uc_dw_auditlog.foobar (
- id int,
- name string,
- age int,
- gender string )
- COMMENT 'Test table'
- ROW FORMAT DELIMITED
- FIELDS TERMINATED BY ',';
+--CREATE EXTERNAL TABLE OVER DATA
+drop table if exists dwx_audit_transition.calc_parts_snapshot;
+create external table dwx_audit_transition.calc_parts_snapshot (val string) stored as textfile
+location 's3://${s3_published_bucket}/analytical-dataset/archive/11_2022_backup/calculationParts/';
+
+--CREATE BASE TABLE WITH ALL DATA FOR DAY INCLUDING DUPES
+drop table if exists dwx_audit_transition.calc_parts_snapshot_enriched;
+create table dwx_audit_transition.calc_parts_snapshot_enriched (id_key string, dbType STRING, json STRING);
+insert into dwx_audit_transition.calc_parts_snapshot_enriched
+select
+concat(get_json_object(val, '$._id.id'),"_",get_json_object(val, '$._id.type')) as id_key
+,case when get_json_object(val, '$._removedDateTime') is null then "INSERT" else "DELETE" end as dbType
+,val as json
+from dwx_audit_transition.calc_parts_snapshot;
