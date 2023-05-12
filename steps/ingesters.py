94bcac959b697879a50c176732cc51c83d90c388
tobywalls <60394163+tobywalls@users.noreply.github.com>
2023-05-12 13:08:33 +0100

    evenly distribute data (#188)
    Co-authored-by: Toby Walls <toby.walls@engineering.digital.dwp.gov.ukDD>

--- steps/ingesters.py
+++ steps/ingesters.py
@@ -551,9 +551,15 @@ class CalcPartBenchmark:
         ])
 
         df = self._spark_session.read.schema(schema).orc(s3_source_url)
-        df.rdd.map(lambda x: x["json"]).repartition(55000).saveAsTextFile(
-            s3_destination_url,
-            compressionCodecClass="com.hadoop.compression.lzo.LzopCodec"
+        (
+            df
+            .repartitionByRange(40000, "id_part", "id_key")
+            .rdd
+            .map(lambda x: x["json"])
+            .saveAsTextFile(
+                s3_destination_url,
+                compressionCodecClass="com.hadoop.compression.lzo.LzopCodec"
+            )
         )
 
     def publish_calculation_parts_sql(self):
