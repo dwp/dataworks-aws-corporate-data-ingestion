53a2dabb0914cb69912a89c6ae1d39f8d5cb4ec1
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2023-06-06 13:40:25 +0100

    fix: terraform deprecation warning

--- bootstrap_actions.tf
+++ bootstrap_actions.tf
@@ -103,7 +103,7 @@ resource "aws_s3_object" "logging_script" {
   }
 }
 
-resource "aws_s3_bucket_object" "patch_log4j_emr_sh" {
+resource "aws_s3_object" "patch_log4j_emr_sh" {
   bucket  = data.terraform_remote_state.common.outputs.config_bucket.id
   key     = "component/${local.emr_cluster_name}/patch-log4j-emr-6.4.0-v2.sh"
   content = file("${path.module}/bootstrap_actions/patch-log4j-emr-6.4.0-v2.sh")
