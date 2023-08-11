33abf220ee0a1f1153f2d65336845efd729a3594
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-08-11 08:24:30 +0100

    chore: Tanium sg and vpce rules

--- security_groups.tf
+++ security_groups.tf
@@ -100,43 +100,43 @@ resource "aws_security_group_rule" "ingress_internet_proxy" {
 }
 
 resource "aws_security_group_rule" "cdi_host_outbound_tanium_1" {
-  description       = "CDI host outbound port 1 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
+  description              = "CDI host outbound port 1 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "cdi_host_outbound_tanium_2" {
-  description       = "CDI host outbound port 2 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
+  description              = "CDI host outbound port 2 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "cdi_host_inbound_tanium_1" {
-  description       = "CDI host inbound port 1 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
+  description              = "CDI host inbound port 1 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
 }
 
 resource "aws_security_group_rule" "cdi_host_inbound_tanium_2" {
-  description       = "CDI host inbound port 2 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
+  description              = "CDI host inbound port 2 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.dataworks_aws_corporate_data_ingestion_common.id
 }
 
 resource "aws_security_group_rule" "egress_dataworks_aws_corporate_data_ingestion_to_dks" {
